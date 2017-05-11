5d806c268975ca5d444f750fd5ace440aa2da1e4
padungsin <padungsin@MARLO10>
2017-05-11 14:30:46 +0700

    Very Plain Version of Spring cloud

--- user-service/src/main/java/au/com/marlo/springcloud/service/UserService.java
+++ user-service/src/main/java/au/com/marlo/springcloud/service/UserService.java
@@ -1,38 +1,6 @@
 package au.com.marlo.springcloud.service;
 
-import java.util.List;
-
-import au.com.marlo.springcloud.model.User;
-
 public interface UserService {
 
-	/**
-	 * Finds account by given name
-	 *
-	 * @param accountName
-	 * @return found account
-	 */
-	User findByLogin(String login);
-
-	/**
-	 * Checks if account with the same name already exists Invokes Auth Service
-	 * user creation Creates new account with default parameters
-	 *
-	 * @param user
-	 * @return created account
-	 */
-	User create(User user);
-
-	/**
-	 * Validates and applies incoming account updates Invokes Statistics Service
-	 * update
-	 *
-	 * @param name
-	 * @param update
-	 */
-	User saveChanges(User user);
-	
-	List<User> listAll();
-	
-	String hello();
+	String sayHello();
 }
