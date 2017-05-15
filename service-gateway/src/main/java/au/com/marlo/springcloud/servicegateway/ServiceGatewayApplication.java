c0f09f6de1d854386471b7d9c5de12a80a75fe1a
padungsin <padungsin@MARLO10>
2017-05-15 14:43:17 +0700

    pending gateway-eureka

--- service-gateway/src/main/java/au/com/marlo/springcloud/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/au/com/marlo/springcloud/servicegateway/ServiceGatewayApplication.java
@@ -2,7 +2,7 @@ package au.com.marlo.springcloud.servicegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -10,7 +10,7 @@ import au.com.marlo.springcloud.servicegateway.filter.pre.SimpleFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class ServiceGatewayApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/au/com/marlo/springcloud/UserServiceApplication.java
+++ user-service/src/main/java/au/com/marlo/springcloud/UserServiceApplication.java
@@ -3,9 +3,11 @@ package au.com.marlo.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
